b18f78d8ef5031331c8088d683e8a1a37a2b7dcc
Cornelius-BobCat <pro.cornelius@gmail.com>
2024-07-24 16:36:09 +0200

    A1

---
+++ app/actions/save.action.ts
@@ -0,0 +1,32 @@
+"use server";
+import { db } from "@/lib/db";
+export const Save = async (pseudo: string, score: number) => {
+  try {
+    // Rechercher le joueur par son pseudo
+    const player = await db.player.findUnique({
+      where: { pseudo },
+    });
+
+    if (player) {
+      // Comparer le score actuel avec le nouveau score
+      if (score > player.score) {
+        // Mettre à jour le score si le nouveau est plus élevé
+        await db.player.update({
+          where: { pseudo },
+          data: { score },
+        });
+        console.log(`Score updated for player ${pseudo}`);
+      } else {
+        console.log(`No update needed for player ${pseudo}`);
+      }
+    } else {
+      // Si le joueur n'existe pas, vous pouvez choisir de le créer
+      await db.player.create({
+        data: { pseudo, score },
+      });
+      console.log(`New player created: ${pseudo}`);
+    }
+  } catch (error) {
+    console.error("Error saving score:", error);
+  }
+};
