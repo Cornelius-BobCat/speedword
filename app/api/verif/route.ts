c4c8572723fda3e177c2ea514ee29b3c93fe51f0
Cornelius-BobCat <pro.cornelius@gmail.com>
2024-07-24 14:10:54 +0200

    a

--- app/api/verif/route.ts
+++ app/api/verif/route.ts
@@ -3,6 +3,24 @@ import { NextRequest, NextResponse } from "next/server";
 import fs from "fs";
 import path from "path";
 
+// Variable pour stocker le cache
+let wordCache: Set<string> | null = null;
+const cacheFilePath = path.join(process.cwd(), "public", "ods6.txt");
+
+async function loadWordCache() {
+  if (!wordCache) {
+    try {
+      const data = fs.readFileSync(cacheFilePath, "utf-8");
+      wordCache = new Set(
+        data.split("\n").map((line) => line.trim().toUpperCase())
+      );
+    } catch (error) {
+      console.error("Error reading file for cache:", error);
+      throw new Error("Failed to read file for cache");
+    }
+  }
+}
+
 export async function GET(req: NextRequest) {
   // Extraire l'URL de la requête
   const url = new URL(req.url);
@@ -10,22 +28,19 @@ export async function GET(req: NextRequest) {
   // Récupérer le paramètre 'word' depuis l'URL de la requête
   const word = (url.searchParams.get("word") as string).toUpperCase();
   console.log(word);
+
   try {
-    // Chemin du fichier
-    const filePath = path.join(process.cwd(), "public", "ods6.txt");
+    await loadWordCache(); // Charger le cache si nécessaire
 
-    // Lire le fichier
-    const data = fs.readFileSync(filePath, "utf-8");
-
-    // Diviser les lignes
-    const lines = data.split("\n").map((line) => line.trim().toUpperCase());
-
-    // Vérifier si le mot est dans les lignes
-    const isWordValid = lines.includes(word);
+    // Vérifier si le mot est dans le cache
+    const isWordValid = wordCache?.has(word) || false;
     console.log(isWordValid);
     return NextResponse.json({ content: isWordValid });
   } catch (error) {
-    console.error("Error reading file:", error);
-    return NextResponse.json({ error: "Failed to read file" }, { status: 500 });
+    console.error("Error processing request:", error);
+    return NextResponse.json(
+      { error: "Failed to process request" },
+      { status: 500 }
+    );
   }
 }
