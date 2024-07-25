2aab5e0c889802bd49f360d99b777bd86d6a4a8a
Cornelius-BobCat <pro.cornelius@gmail.com>
2024-07-25 12:10:54 +0200

    A

--- app/score/page.tsx
+++ app/score/page.tsx
@@ -1,48 +1,38 @@
-// pages/scores.tsx
-
 import Link from "next/link";
 import { GetScore } from "../actions/getscore.action";
-import { GetServerSideProps } from "next";
+import Image from "next/image";
+/**
+ * Renders the Page component.
+ * Retrieves the score data and displays it in a formatted list.
+ * @returns The rendered Page component.
+ */
 
-type Score = {
-  pseudo: string;
-  score: number;
-};
+export default async function Page() {
+  const res = await GetScore();
+  console.log(res);
 
-type Props = {
-  scores: Score[];
-};
-
-export default function ScoresPage({ scores }: Props) {
   return (
-    <div className="w-3/12 mx-auto text-2xl items-center text-center space-y-3 ">
-      <div className="py-10">
-        <Link
-          href="/"
-          className="text-2xl text-white bg-black rounded-lg py-5 px-10 hover:bg-black/50"
-        >
-          🎯 GO BACK
-        </Link>
-      </div>
-      {scores.map((score, index) => (
-        <div
-          key={index}
-          className="flex justify-between bg-black/50 text-white p-2"
-        >
-          <div className="font-bold">{score.pseudo}</div>
-          <div>{score.score} pts</div>
+    <>
+      <div className="w-3/12 mx-auto text-2xl items-center text-center space-y-3 z-30">
+        <div className="py-10">
+          <Link
+            href="/"
+            className="text-2xl  text-white bg-black rounded-lg py-5 px-10 hover:bg-black/50"
+          >
+            🎯 GO BACK
+          </Link>
         </div>
-      ))}
-    </div>
+        {res.map((score, index) => (
+          <div
+            key={index}
+            className="flex justify-between bg-black/50 text-white p-2"
+          >
+            <div className="font-bold">{score.pseudo}</div>
+
+            <div>{score.score} pts</div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const scores = await GetScore();
-
-  return {
-    props: {
-      scores,
-    },
-  };
-};
